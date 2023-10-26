2cf29e927ed4fc3e7722e802f227f61927ac2c85
Jose Angel Gomez Bravo <04josegomez2022@gmail.com>
2023-10-25 19:23:27 -0700

    commit java code

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
-// boolean example
+/* boolean example
 
 let variable1 = false;
 console.log(variable1);
 
 // null example
-let variable = null;
-console.log(variable);
+let variable2 = null;
+console.log(variable2);
+
+// undefined example
+let variable3;
+console.log(variable3);
+
+// numbers 
+let variable4 = 25;
+console.log(variable4);
+
+let variable5 = "25"
+console.log(variable5);
+
+let variable6 = 25.2e10;
+console.log(variable6);
+
+// strings 
+let variable7 = "Hello World!"
+console.log(variable7);
+*/
+
+console.log(8 * null);
+
+console.log("5"- 1);
+
+console.log("5" + 1);
+
+console.log("five" * 2);
+
+console.log(false == 0);
+
+console.log(6.5 > 2 && 6> 3);
+
+console.log(false == false ? "equal" : "not equal");
+
+console.log(true || false);
